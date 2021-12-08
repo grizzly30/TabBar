8f2bdcb54903f1e26c9a3003cb9f6c348ddea6f5
“grizzly30” <mihajovanovic30@gmail.com>
2021-12-08 11:47:42 +0100

    Initial Commit

---
+++ Vezba/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vezba
+//
+//  Created by Mihailo Jovanovic on 8.12.21..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
